47c7d6f15f3823d24bbd70b578b6c420948fb6a0
Ahmed Samir <a.samir9710@gmail.com>
2020-11-17 16:42:46 +0200

    edited static files settings

--- charity/settings.py
+++ charity/settings.py
@@ -40,15 +40,6 @@ elif os.getenv("PRODUCTION", None):
     prod_db  =  dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(prod_db)
 
-    PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-    STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATIC_URL = '/static/'
-
-    # Extra lookup directories for collectstatic to find static files
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
 
 # for development 
 else:
@@ -163,4 +154,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CORS_ALLOW_ALL_ORIGINS = True
