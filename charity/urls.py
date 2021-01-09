97a69ffd0370a280b0c49e1ae53a91f7ce5616e1
Ahmed Samir <a.samir9710@gmail.com>
2021-01-09 04:23:22 +0200

    edited images path

--- charity/urls.py
+++ charity/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('api/replies/', include('comments.replies_urls', namespace='comments')),
 
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
