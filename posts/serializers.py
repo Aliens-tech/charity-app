670ecd848455f9b0615cefdb36da0d1c1fac2c7d
Ahmed Samir <a.samir9710@gmail.com>
2021-01-15 15:45:34 +0200

    edited image field serializer to images_list

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Post, Category, PostImage
 
+import json
+
 class PostImageSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField()
 
@@ -16,13 +18,12 @@ class PostSerializer(serializers.ModelSerializer):
     
     categories = serializers.SerializerMethodField()
     user = serializers.StringRelatedField()
-    images = serializers.SerializerMethodField()
-    image_item = serializers.SerializerMethodField()
+    images_list = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ("id", "categories", "user", "images", "title", "description", 'city', 'region', 'created_at', 'price', 'image_item') 
+        fields = ("id", "categories", "user", "images_list", "title", "description", 'city', 'region', 'created_at', 'price') 
 
     def get_categories(self, obj):
         post_categories = ''
@@ -30,17 +31,13 @@ class PostSerializer(serializers.ModelSerializer):
             post_categories +='#%s '%(category.name)
         return post_categories
 
-    def get_image_item(self, obj):
-        first_image = obj.images.first()
-        if first_image:
-            serializer = PostImageSerializer(first_image)
-            return serializer.data['img']
-        else:
-            return None
-
-    def get_images(self, obj):
-        serializer = PostImageSerializer(obj.images.all(), many=True)
-        return serializer.data
+    def get_images_list(self, obj):
+        images_list  = obj.images.all()
+        images = []
+        for image in images_list:
+            images.append(image.img.url)
+        
+        return images
 
     def get_created_at(self, obj):
         return obj.created_at.strftime("%m/%d/%Y, %H:%M:%S")
